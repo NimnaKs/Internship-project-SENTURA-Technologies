627792ec4aba209661d9309fa2489102d770f428
NimnaKs <nimnakaveesha123456@gmail.com>
2024-08-25 09:05:25 +0530

    Init the project

---
+++ src/main/java/lk/ijse/weavyapiintegration/WeavyApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.weavyapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavyApiIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeavyApiIntegrationApplication.class, args);
+    }
+
+}
